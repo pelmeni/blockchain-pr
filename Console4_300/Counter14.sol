d8a0e7183032ab054d43fcfd6bd8aedb9dad4117
Sergey Pichugin <pichuginsergey@hotmail.com>
2019-11-02 00:14:43 +0300

    fix errorsfix

--- Console4_300/Counter14.sol
+++ Console4_300/Counter14.sol
@@ -263,14 +263,14 @@ contract Counter14  {
       userData[userAddress].index);
     } 
     
-    int private count = 0;
-    function incrementCounter() public {
-        count += 1;
-    }
-    function decrementCounter() public {
-        count -= 1;
-    }
-    function getCount() public view returns (int) {
-        return count;
-    }
+    // int private count = 0;
+    // function incrementCounter() public {
+    //     count += 1;
+    // }
+    // function decrementCounter() public {
+    //     count -= 1;
+    // }
+    // function getCount() public view returns (int) {
+    //     return count;
+    // }
 }
